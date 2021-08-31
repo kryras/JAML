594a53abce2b5dbc8d3a1d77a7232f4c51caaedd
kryras <v4m4n4@gmail.com>
2021-08-31 18:06:35 +0200

    change to hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -47,7 +47,7 @@ const routes = [
 
 const router = createRouter({
   // history: createWebHistory(process.env.BASE_URL),
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
