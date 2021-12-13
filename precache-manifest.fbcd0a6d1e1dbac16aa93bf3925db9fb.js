58e72d893bf69dd46570ae5331ba70062c94e30e
kryras <v4m4n4@gmail.com>
2021-12-13 01:03:17 +0100

    'deploy'

---
+++ precache-manifest.fbcd0a6d1e1dbac16aa93bf3925db9fb.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3c3ab898c9a598d09d5",
+    "url": "/JAML/css/404.53bd7f03.css"
+  },
+  {
+    "revision": "5bb7d3cf777d3352de2d",
+    "url": "/JAML/css/about.c91d452d.css"
+  },
+  {
+    "revision": "e1d60d4fa38bdfe61ed7",
+    "url": "/JAML/css/app.21725bed.css"
+  },
+  {
+    "revision": "7aa2e66f254683fe2c2e",
+    "url": "/JAML/css/hiragana.c4a624cd.css"
+  },
+  {
+    "revision": "56aeb064f7bb879df08c",
+    "url": "/JAML/css/kanalesson.a28047bf.css"
+  },
+  {
+    "revision": "40cc1de6117a7572648e",
+    "url": "/JAML/css/kanalesson~search.e3bfc240.css"
+  },
+  {
+    "revision": "bcf6d89a95af0565b50f",
+    "url": "/JAML/css/kanji.2849b8c2.css"
+  },
+  {
+    "revision": "25ff2b3dfa67dd704660",
+    "url": "/JAML/css/katakana.c2263436.css"
+  },
+  {
+    "revision": "9ee94885eb30440ea74e",
+    "url": "/JAML/css/search.0d008b20.css"
+  },
+  {
+    "revision": "9506e17f27690b66dc087627ce0d6be8",
+    "url": "/JAML/img/background.9506e17f.svg"
+  },
+  {
+    "revision": "e2a5c18f443fac140a0771fdc4182f3a",
+    "url": "/JAML/img/background_desktop.e2a5c18f.svg"
+  },
+  {
+    "revision": "a4b5ad779669f10e3b715af6e146a036",
+    "url": "/JAML/img/logo.a4b5ad77.png"
+  },
+  {
+    "revision": "ce2c024a45aa9cf8224253e703b2ddb1",
+    "url": "/JAML/img/logo_hiragana.ce2c024a.svg"
+  },
+  {
+    "revision": "106958cdc4f3c292766f23f546ae1630",
+    "url": "/JAML/img/logo_kanji.106958cd.svg"
+  },
+  {
+    "revision": "8ca4d57c9eb485be4760b970eb90d1c5",
+    "url": "/JAML/img/logo_katakana.8ca4d57c.svg"
+  },
+  {
+    "revision": "f1428696cde18711bba0fbf5f27ce3b1",
+    "url": "/JAML/index.html"
+  },
+  {
+    "revision": "e3c3ab898c9a598d09d5",
+    "url": "/JAML/js/404.55c5a617.js"
+  },
+  {
+    "revision": "5bb7d3cf777d3352de2d",
+    "url": "/JAML/js/about.6ac53328.js"
+  },
+  {
+    "revision": "e1d60d4fa38bdfe61ed7",
+    "url": "/JAML/js/app.f494e01f.js"
+  },
+  {
+    "revision": "643972b5e3419c059d2c",
+    "url": "/JAML/js/chunk-vendors.230d6ff9.js"
+  },
+  {
+    "revision": "7aa2e66f254683fe2c2e",
+    "url": "/JAML/js/hiragana.aef8550a.js"
+  },
+  {
+    "revision": "56aeb064f7bb879df08c",
+    "url": "/JAML/js/kanalesson.963a516b.js"
+  },
+  {
+    "revision": "40cc1de6117a7572648e",
+    "url": "/JAML/js/kanalesson~search.c8d48e8d.js"
+  },
+  {
+    "revision": "bcf6d89a95af0565b50f",
+    "url": "/JAML/js/kanji.5c582060.js"
+  },
+  {
+    "revision": "25ff2b3dfa67dd704660",
+    "url": "/JAML/js/katakana.07865c10.js"
+  },
+  {
+    "revision": "9ee94885eb30440ea74e",
+    "url": "/JAML/js/search.86036acd.js"
+  },
+  {
+    "revision": "bf47a95c76eb01801312983710d89c89",
+    "url": "/JAML/js/tf-worker.f19f4451.worker.js"
+  },
+  {
+    "revision": "9fec6a99e1c9bb14aac67c6e6078f1b7",
+    "url": "/JAML/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/JAML/robots.txt"
+  }
+]);
